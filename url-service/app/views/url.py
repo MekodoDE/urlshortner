98b3bcce0b2b1d20706c6137980af7c95e3b01ac
Marco <Marco@TOKYO>
2024-09-10 22:15:42 +0200

    Remove unnecessary imports

--- url-service/app/views/url.py
+++ url-service/app/views/url.py
@@ -1,5 +1,4 @@
 import uuid
-from flask import request, jsonify
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
